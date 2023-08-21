462cfa323e2c03b1453328c7a80315fd6561143f
hellenwamaitha <hellenwama254@gmail.com>
2023-08-21 14:54:44 +0300

    updated lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+d1= Dog("fibo")
+
+print(d1.name)
+print(d1.breed)
